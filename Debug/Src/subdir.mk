384b75d5bc26a97f67ce71c55f2177faa55fb897
James Graham <james.graham40@yahoo.co.uk>
2024-01-23 10:17:24 +0000

    Added LVGL

--- Debug/Src/subdir.mk
+++ Debug/Src/subdir.mk
@@ -40,7 +40,7 @@ C_DEPS += \
 
 # Each subdirectory must supply rules for building sources it contributes
 Src/%.o Src/%.su Src/%.cyclo: ../Src/%.c Src/subdir.mk
-	arm-none-eabi-gcc "$<" -mcpu=cortex-m4 -std=gnu11 -g3 -DDEBUG -DSTM32F401CCUx -DSTM32F401xC -DSTM32 -DSTM32F4 -c -I../Inc -I"C:/Users/User/Documents/Code_Library/Trident_Repo/GC9A01_Display/chip_headers" -I"C:/Users/User/Documents/Code_Library/Trident_Repo/GC9A01_Display/chip_headers/CMSIS/Device/ST/STM32F4xx/Include" -I"C:/Users/User/Documents/Code_Library/Trident_Repo/GC9A01_Display/chip_headers/CMSIS/Include" -O0 -ffunction-sections -fdata-sections -Wall -fstack-usage -fcyclomatic-complexity -MMD -MP -MF"$(@:%.o=%.d)" -MT"$@" --specs=nano.specs -mfpu=fpv4-sp-d16 -mfloat-abi=hard -mthumb -o "$@"
+	arm-none-eabi-gcc "$<" -mcpu=cortex-m4 -std=gnu11 -g3 -DDEBUG -DSTM32F401CCUx -DSTM32F401xC -DSTM32 -DSTM32F4 -c -I../Inc -I"C:/Users/Administrator/git/GC9A01A_Driver_Demo/chip_headers" -I"C:/Users/Administrator/git/GC9A01A_Driver_Demo/chip_headers/CMSIS/Device/ST/STM32F4xx/Include" -I"C:/Users/Administrator/git/GC9A01A_Driver_Demo/chip_headers/CMSIS/Include" -O0 -ffunction-sections -fdata-sections -Wall -fstack-usage -fcyclomatic-complexity -MMD -MP -MF"$(@:%.o=%.d)" -MT"$@" --specs=nano.specs -mfpu=fpv4-sp-d16 -mfloat-abi=hard -mthumb -o "$@"
 
 clean: clean-Src
 
--- Debug/Src/trident_spi_f4.d
+++ Debug/Src/trident_spi_f4.d
@@ -1,21 +1,21 @@
 Src/trident_spi_f4.o: ../Src/trident_spi_f4.c ../Inc/trident_spi_f4.h \
- C:/Users/User/Documents/Code_Library/Trident_Repo/GC9A01_Display/chip_headers/CMSIS/Device/ST/STM32F4xx/Include/stm32f4xx.h \
- C:/Users/User/Documents/Code_Library/Trident_Repo/GC9A01_Display/chip_headers/CMSIS/Device/ST/STM32F4xx/Include/stm32f401xc.h \
- C:/Users/User/Documents/Code_Library/Trident_Repo/GC9A01_Display/chip_headers/CMSIS/Include/core_cm4.h \
- C:/Users/User/Documents/Code_Library/Trident_Repo/GC9A01_Display/chip_headers/CMSIS/Include/cmsis_version.h \
- C:/Users/User/Documents/Code_Library/Trident_Repo/GC9A01_Display/chip_headers/CMSIS/Include/cmsis_compiler.h \
- C:/Users/User/Documents/Code_Library/Trident_Repo/GC9A01_Display/chip_headers/CMSIS/Include/cmsis_gcc.h \
- C:/Users/User/Documents/Code_Library/Trident_Repo/GC9A01_Display/chip_headers/CMSIS/Include/mpu_armv7.h \
- C:/Users/User/Documents/Code_Library/Trident_Repo/GC9A01_Display/chip_headers/CMSIS/Device/ST/STM32F4xx/Include/system_stm32f4xx.h \
+ C:/Users/Administrator/git/GC9A01A_Driver_Demo/chip_headers/CMSIS/Device/ST/STM32F4xx/Include/stm32f4xx.h \
+ C:/Users/Administrator/git/GC9A01A_Driver_Demo/chip_headers/CMSIS/Device/ST/STM32F4xx/Include/stm32f401xc.h \
+ C:/Users/Administrator/git/GC9A01A_Driver_Demo/chip_headers/CMSIS/Include/core_cm4.h \
+ C:/Users/Administrator/git/GC9A01A_Driver_Demo/chip_headers/CMSIS/Include/cmsis_version.h \
+ C:/Users/Administrator/git/GC9A01A_Driver_Demo/chip_headers/CMSIS/Include/cmsis_compiler.h \
+ C:/Users/Administrator/git/GC9A01A_Driver_Demo/chip_headers/CMSIS/Include/cmsis_gcc.h \
+ C:/Users/Administrator/git/GC9A01A_Driver_Demo/chip_headers/CMSIS/Include/mpu_armv7.h \
+ C:/Users/Administrator/git/GC9A01A_Driver_Demo/chip_headers/CMSIS/Device/ST/STM32F4xx/Include/system_stm32f4xx.h \
  ../Inc/trident_timebase.h ../Inc/trident_gpio_f4.h
 ../Inc/trident_spi_f4.h:
-C:/Users/User/Documents/Code_Library/Trident_Repo/GC9A01_Display/chip_headers/CMSIS/Device/ST/STM32F4xx/Include/stm32f4xx.h:
-C:/Users/User/Documents/Code_Library/Trident_Repo/GC9A01_Display/chip_headers/CMSIS/Device/ST/STM32F4xx/Include/stm32f401xc.h:
-C:/Users/User/Documents/Code_Library/Trident_Repo/GC9A01_Display/chip_headers/CMSIS/Include/core_cm4.h:
-C:/Users/User/Documents/Code_Library/Trident_Repo/GC9A01_Display/chip_headers/CMSIS/Include/cmsis_version.h:
-C:/Users/User/Documents/Code_Library/Trident_Repo/GC9A01_Display/chip_headers/CMSIS/Include/cmsis_compiler.h:
-C:/Users/User/Documents/Code_Library/Trident_Repo/GC9A01_Display/chip_headers/CMSIS/Include/cmsis_gcc.h:
-C:/Users/User/Documents/Code_Library/Trident_Repo/GC9A01_Display/chip_headers/CMSIS/Include/mpu_armv7.h:
-C:/Users/User/Documents/Code_Library/Trident_Repo/GC9A01_Display/chip_headers/CMSIS/Device/ST/STM32F4xx/Include/system_stm32f4xx.h:
+C:/Users/Administrator/git/GC9A01A_Driver_Demo/chip_headers/CMSIS/Device/ST/STM32F4xx/Include/stm32f4xx.h:
+C:/Users/Administrator/git/GC9A01A_Driver_Demo/chip_headers/CMSIS/Device/ST/STM32F4xx/Include/stm32f401xc.h:
+C:/Users/Administrator/git/GC9A01A_Driver_Demo/chip_headers/CMSIS/Include/core_cm4.h:
+C:/Users/Administrator/git/GC9A01A_Driver_Demo/chip_headers/CMSIS/Include/cmsis_version.h:
+C:/Users/Administrator/git/GC9A01A_Driver_Demo/chip_headers/CMSIS/Include/cmsis_compiler.h:
+C:/Users/Administrator/git/GC9A01A_Driver_Demo/chip_headers/CMSIS/Include/cmsis_gcc.h:
+C:/Users/Administrator/git/GC9A01A_Driver_Demo/chip_headers/CMSIS/Include/mpu_armv7.h:
+C:/Users/Administrator/git/GC9A01A_Driver_Demo/chip_headers/CMSIS/Device/ST/STM32F4xx/Include/system_stm32f4xx.h:
 ../Inc/trident_timebase.h:
 ../Inc/trident_gpio_f4.h:
