231cb4efefd236d51d05d1fd10cc825495cab07d
James Graham <james.graham@tridentssl.co.uk>
2024-01-23 14:31:06 +0000

    Updated driver

--- Src/main.c
+++ Src/main.c
@@ -23,7 +23,7 @@
 #include "GC9A01.h"
 #include "math.h"
 
-#include "lv_port_disp.h"
+
 
 /************************************
  * EXTERN VARIABLES
@@ -115,19 +115,130 @@ int main(void)
 	GPIO_Configure();
 
 	/*Init the display*/
-	lv_port
+	GC9A01_init();
 
 	/* Loop forever */
 	while (1)
-	{
+		{
+
+			uint8_t color[3];
+			// Triangle
+			color[0] = 0xFF;
+			color[1] = 0xFF;
+			for (int x = 0; x < 240; x++)
+			{
+				for (int y = 0; y < 240; y++)
+				{
+					if (x < y)
+					{
+						color[2] = 0xFF;
+					}
+					else
+					{
+						color[2] = 0x00;
+					}
+					if (x == 0 && y == 0)
+					{
+						GC9A01_write(color, sizeof(color));
+					}
+					else
+					{
+						GC9A01_write_continue(color, sizeof(color));
+					}
+				}
+			}
+
+			Time_Delay_Ms(500);
+
+			// Rainbow
+			float frequency = 0.026;
+			for (int x = 0; x < 240; x++)
+			{
+				color[0] = sin(frequency * x + 0) * 127 + 128;
+				color[1] = sin(frequency * x + 2) * 127 + 128;
+				color[2] = sin(frequency * x + 4) * 127 + 128;
+				for (int y = 0; y < 240; y++)
+				{
+					if (x == 0 && y == 0)
+					{
+						GC9A01_write(color, sizeof(color));
+					}
+					else
+					{
+						GC9A01_write_continue(color, sizeof(color));
+					}
+				}
+			}
+
+			Time_Delay_Ms(500);
+
+			// Checkerboard
+			for (int x = 0; x < 240; x++)
+			{
+				for (int y = 0; y < 240; y++)
+				{
+					if ((x / 10) % 2 == (y / 10) % 2)
+					{
+						color[0] = 0xFF;
+						color[1] = 0xFF;
+						color[2] = 0xFF;
+					}
+					else
+					{
+						color[0] = 0x00;
+						color[1] = 0x00;
+						color[2] = 0x00;
+					}
+					if (x == 0 && y == 0)
+					{
+						GC9A01_write(color, sizeof(color));
+					}
+					else
+					{
+						GC9A01_write_continue(color, sizeof(color));
+					}
+				}
+			}
+
+			Time_Delay_Ms(1000);
+
+			// Blue cross
+			color[0] = 0x25;
+			for (int x = 0; x < 240; x++)
+			{
+				for (int y = 0; y < 240; y++)
+				{
+					if ((x >= 1 * 48 && x < 4 * 48 && y >= 2 * 48 && y < 3 * 48)
+							|| (x >= 2 * 48 && x < 3 * 48 && y >= 1 * 48
+									&& y < 4 * 48))
+					{
+						color[1] = 0x96;
+						color[2] = 0xBE;
+					}
+					else
+					{
+						color[1] = 0x00;
+						color[2] = 0x00;
+					}
+					if (x == 0 && y == 0)
+					{
+						GC9A01_write(color, sizeof(color));
+					}
+					else
+					{
+						GC9A01_write_continue(color, sizeof(color));
+					}
+				}
+			}
 
 
+			Time_Delay_Ms(1000);
 
-		/* USER CODE END WHILE */
+			/* USER CODE END WHILE */
 
-		/* USER CODE BEGIN 3 */
+			/* USER CODE BEGIN 3 */
+		}
 	}
-}
 
 /**
  * @brief  Retargets the C library printf function to the USART.
